259bcbc3217f9a42469613e1b67cf8ee2a33c325
EpicKarl <73167981+EpicKarl@users.noreply.github.com>
2021-01-13 11:32:29 +0100

    Update sendUarttest.ino

--- Codes/sendDummy/sendUarttest.ino
+++ Codes/sendDummy/sendUarttest.ino
@@ -1,76 +1,151 @@
 
-byte dataToSend[10] = {0};
+byte dataToSend[10] = {'0','0','0','0','0','0','0','0','0','0'};
+byte dataRecieve[6] = {'0','0','0','0','0','0'};
+byte bufferData[6] = {'0','0','0','0','0','0'};
+byte incomingSignal;
 int OldState;
 int count;
 
-void sendDataToSerial(){		//Dummy, der alle 2 Sekunden das Sensorabbild wechsel
-      switch(count){			//A000000000
-        case 1:					//B100001100
-          dataToSend[0]= 'A';	//C010011010
-          dataToSend[1]= 0;		//D001001001
-          dataToSend[2]= 0;		//Nachrichtlänge variiert also, muss bei empfang angepasst werden
-          dataToSend[3]= 0;
-          dataToSend[4]= 0;
-          dataToSend[5]= 0;
-          dataToSend[6]= 0;
-		  dataToSend[7]= 0;
-		  dataToSend[8]= 0;
-		  dataToSend[9]= 0;
-          break;
-        case 2:
-          dataToSend[0]= 'B';
-          dataToSend[1]= 1;
-          dataToSend[2]= 0;
-          dataToSend[3]= 0;
-          dataToSend[4]= 0;
-          dataToSend[5]= 0;
-          dataToSend[6]= 1;
-		  dataToSend[7]= 1;
-		  dataToSend[8]= 0;
-		  dataToSend[9]= 0;
-          break;
-        case 3:
-          dataToSend[0]= 'C';
-          dataToSend[1]= 0;
-          dataToSend[2]= 1;
-          dataToSend[3]= 0;
-          dataToSend[4]= 0;
-          dataToSend[5]= 1;
-          dataToSend[6]= 1;		  
-		  dataToSend[7]= 0;
-		  dataToSend[8]= 1;
-		  dataToSend[9]= 0;
-          break;
-        case 4:
-          dataToSend[0]= 'D';
-          dataToSend[1]= 0;
-          dataToSend[2]= 0;
-          dataToSend[3]= 1;
-          dataToSend[4]= 0;
-          dataToSend[5]= 0;
-          dataToSend[6]= 1;		  
-		  dataToSend[7]= 0;
-		  dataToSend[8]= 0;
-		  dataToSend[9]= 1;
-          count = 0;
-          break;
-          default:
-          break;
-      }
-      if(dataToSend[0] != OldState){ //array wird nur gesendet, wenn sich dataToSend[0] --der state-- verändert
-      for (int x = 0; x<10; x++){	 //verwendet Arduinogruppe so beinhaltet aber, dass FSM korrekt funktioniert
-        Serial.print(dataToSend[x]); 
-      }
-      OldState = dataToSend[0];
-    }
+void sendDataToSerial(){
+	if(dataRecieve[0] == 0){
+		switch(count){			
+			case 1:					
+			  dataToSend[0]= 'D';	
+			  dataToSend[1]= 0;		
+			  dataToSend[2]= 0;		
+			  dataToSend[3]= 0;
+			  dataToSend[4]= 0;
+			  dataToSend[5]= 0;
+			  dataToSend[6]= 0;
+			  dataToSend[7]= 0;
+			  dataToSend[8]= 0;
+			  dataToSend[9]= 0;
+			  break;
+			case 2:
+			  dataToSend[0]= 'H';
+			  dataToSend[1]= 0;
+			  dataToSend[2]= 1;
+			  dataToSend[3]= 0;
+			  dataToSend[4]= 0;
+			  dataToSend[5]= 1;
+			  dataToSend[6]= 0;
+			  dataToSend[7]= 0;
+			  dataToSend[8]= 0;
+			  dataToSend[9]= 0;
+			  break;
+			case 3:
+			  dataToSend[0]= 'J';
+			  dataToSend[1]= 0;
+			  dataToSend[2]= 0;
+			  dataToSend[3]= 0;
+			  dataToSend[4]= 1;
+			  dataToSend[5]= 0;
+			  dataToSend[6]= 0;		  
+			  dataToSend[7]= 0;
+			  dataToSend[8]= 0;
+			  dataToSend[9]= 0;
+			  break;
+			case 4:
+			  dataToSend[0]= 'F';
+			  dataToSend[1]= 1;
+			  dataToSend[2]= 0;
+			  dataToSend[3]= 0;
+			  dataToSend[4]= 0;
+			  dataToSend[5]= 1;
+			  dataToSend[6]= 0;		  
+			  dataToSend[7]= 0;
+			  dataToSend[8]= 0;
+			  dataToSend[9]= 0;
+			  break;
+			case 5:
+			  dataToSend[0]= 'I';
+			  dataToSend[1]= 0;
+			  dataToSend[2]= 0;
+			  dataToSend[3]= 1;
+			  dataToSend[4]= 0;
+			  dataToSend[5]= 0;
+			  dataToSend[6]= 0;		  
+			  dataToSend[7]= 0;
+			  dataToSend[8]= 0;
+			  dataToSend[9]= 0;
+			  count = 0;
+			  break;
+			  default:
+			  break;
+		}
+	}else if(dataRecieve[0] == 1){
+		if(dataRecieve[4] == 1){
+			dataToSend[0]= 'J';
+			dataToSend[1]= 0;
+			dataToSend[2]= 0;
+			dataToSend[3]= 0;
+			dataToSend[4]= 0;
+			dataToSend[5]= dataRecieve[5];
+			dataToSend[6]= 1;		  
+			dataToSend[7]= 0;
+			dataToSend[8]= 0;
+			dataToSend[9]= 0;
+		}else if(dataRecieve[3] == 1){
+			dataToSend[0]= 'I';
+			dataToSend[1]= 0;
+			dataToSend[2]= 0;
+			dataToSend[3]= 0;
+			dataToSend[4]= 0;
+			dataToSend[5]= dataRecieve[5];
+			dataToSend[6]= 0;		  
+			dataToSend[7]= 1;
+			dataToSend[8]= 0;
+			dataToSend[9]= 0;
+		}else if(dataRecieve[1] == 1){
+			dataToSend[0]= 'F';
+			dataToSend[1]= 0;
+			dataToSend[2]= 0;
+			dataToSend[3]= 0;
+			dataToSend[4]= 0;
+			dataToSend[5]= dataRecieve[5];
+			dataToSend[6]= 0;		  
+			dataToSend[7]= 0;
+			dataToSend[8]= 1;
+			dataToSend[9]= 0;
+		}else if(dataRecieve[2] == 1){
+			dataToSend[0]= 'H';
+			dataToSend[1]= 0;
+			dataToSend[2]= 0;
+			dataToSend[3]= 0;
+			dataToSend[4]= 0;
+			dataToSend[5]= dataRecieve[5];
+			dataToSend[6]= 0;		  
+			dataToSend[7]= 0;
+			dataToSend[8]= 0;
+			dataToSend[9]= 1;
+		}									
+	}
+    if(dataToSend[0] != OldState){ //array wird nur gesendet, wenn sich dataToSend[0] --der state-- verändert
+		for (int x = 0; x<10; x++){	 
+			Serial.print(dataToSend[x]); 
+		}
+		OldState = dataToSend[0];
+	}
+	if(dataRecieve[0] == 0){
+		delay(2000);
+	}
+}
+
+void recieveDataFromSerial(){
+	Serial.setTimeout(100);
+	if(Serial.available()){
+		incomingSignal = Serial.readBytes(bufferData, 6);
+		for(int i = 0; i < 6; i++){
+			dataRecieve[i] = bufferData[i];
+		}
+	}
 }
 
 void setup() {
-  Serial.begin(9600);
+	Serial.begin(9600);
 }
 
 void loop() {
-  sendDataToSerial();
-  count++;
-  delay(2000);
+	sendDataToSerial();
+	recieveDataFromSerial();
 }
