423ce46e525c992b09a423e6c1fa379a9567e0eb
Julius Preuschoff <julius.preuschoff@haw-hamburg.de>
2021-01-13 13:40:51 +0100

    changed COM Ports

--- Fernwartung/src/main.cpp
+++ Fernwartung/src/main.cpp
@@ -7,7 +7,7 @@ Reset Button unnötog, wenn Emergency Button nicht implementiert
 
 #include <Arduino.h>
 #include <ESP8266WebServer.h> 
-#include <ESP8266WiFi.h> 
+#include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <stdlib.h>
 
